1b11f19e21768beade62c0e520d2cf625c32671e
Saifullah <saifullah.cs14@iba-suk.edu.pk>
2018-11-11 21:50:00 +0500

    changed ettings whitenose

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -149,7 +149,7 @@ STATIC_ROOT =  os.path.join(BASE_DIR,'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT =  BASE_DIR
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
