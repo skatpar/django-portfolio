db2a42180a02a14e516d9cf41062ffaa966c2821
Saifullah <saifullah.cs14@iba-suk.edu.pk>
2018-11-12 03:49:30 +0500

    tried new settings for media files

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -154,7 +154,7 @@ STATIC_ROOT =  os.path.join(BASE_DIR,'static')
 
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT =  BASE_DIR
+MEDIA_ROOT = BASE_DIR
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
