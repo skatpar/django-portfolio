0c14408b2ec54d4382ec2867bab1feea69a6b940
Saifullah <saifullah.cs14@iba-suk.edu.pk>
2018-11-11 19:31:56 +0500

    modified hosts server configs

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
 #ALLOWED_HOSTS = ['*']
-ALLOWED_HOSTS = ['ec2-50-19-127-158.compute-1.amazonaws.com']
+ALLOWED_HOSTS = ['fierce-shore-48632.herokuapp.com']
 
 
 # Application definition
