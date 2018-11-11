87cc6582e481f59c90a7e17641c6019aa703a128
Saifullah <saifullah.cs14@iba-suk.edu.pk>
2018-11-11 21:38:00 +0500

    chnaged settings

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -142,9 +142,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
 STATIC_URL = '/static/'
 STATIC_ROOT =  os.path.join(BASE_DIR,'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT =  BASE_DIR
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
