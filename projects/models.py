7340936c50f07d0af9fe09c85d348f2f5e955690
Saifullah <saifullah.cs14@iba-suk.edu.pk>
2018-11-12 01:35:02 +0500

    chaned title and links

--- projects/models.py
+++ projects/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Project(models.Model):
-	title = models.CharField(max_length=20,default='Project')	
+	title = models.CharField(max_length=20,default='Project')
 	image = models.ImageField(upload_to='images/')
 	summary = models.CharField(max_length=200)
 	link = models.CharField(max_length=100,default='#')
 
 	def __str__(self):
-		return self.summary
+		return self.title
