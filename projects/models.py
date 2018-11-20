a1dd304a8adeb09d5733c13e4f8b66a0656ac51c
Saifullah <saifullah.cs14@iba-suk.edu.pk>
2018-11-20 16:56:13 +0500

    removed SSEI image

--- projects/models.py
+++ projects/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Project(models.Model):
-	title = models.CharField(max_length=20,default='Project')
+	title = models.CharField(max_length=40,default='Project')
 	image = models.ImageField(upload_to='images/')
 	summary = models.CharField(max_length=200)
 	link = models.CharField(max_length=100,default='#')
